cd82daa2de21abe1e4f3548c6d02f1dc95ca4802
María Paula Montalto <montaltomariapaul@gmail.com>
2026-05-02 22:40:43 -0300

    Vistas

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, "core/index.html")
+
+def saludar(request):
+    return HttpResponse("Hola desde Django")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse ("<h1>Hola con etiqueta</h1>")
