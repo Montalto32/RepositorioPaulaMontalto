0836d054ab16d571835123a3cd2c946d2a413813
María Paula Montalto <montaltomariapaul@gmail.com>
2026-05-22 14:33:22 -0300

    Crea modelo Categoría y lo registra en admin

--- src/core/models.py
+++ src/core/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
-    descripcion = models.TextField(null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return self.nombre
+
